ec32db7d87737b56382a5448251a2162362c4d3a
Sumesh chandra jena <sumeshjena.co@gmail.com>
2021-08-25 23:24:37 +0530

    Added: Calculate Emp Wage

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -9,3 +9,27 @@
       console.log("UC1 - employee is Present");
     }
 }
+
+//UC 2 using switch case to calculate emp wage
+const IS_PART_TIME = 1;
+const IS_FULL_TIME = 2;
+const PART_TIME_HOURS = 4;
+const FULL_TIME_HOURS = 8;
+const WAGE_PER_HOURS = 20;
+
+{
+  let empHr = 0;
+  let empCheck = Math.floor(Math.random()*10)%3;
+  switch(empCheck){
+    case IS_PART_TIME:
+          empHr = PART_TIME_HOURS;
+          break;
+    case IS_FULL_TIME:
+          empHr = FULL_TIME_HOURS;
+          break;
+    default:
+          empHr = 0;           
+  }
+  let empWage =empHr*WAGE_PER_HOURS;
+  console.log("UC2 - Emp Wage : "+empWage)
+}
