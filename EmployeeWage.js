ada3aa1cafe3ea3cc79cf2923d486df2221417ab
Sumesh chandra jena <sumeshjena.co@gmail.com>
2021-08-26 07:42:09 +0530

    Added: Monthly Wage Calc

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -1,16 +1,16 @@
-// //UC1
-// {
-//     const IS_ABSENT = 0;
-//     let empCheck = Math.floor(Math.random()*10)%2;
-//     if (empCheck==IS_ABSENT){
-//     console.log("UC1 - employee is Absent. Exiting the program");
-//     return;
-//     }else{
-//       console.log("UC1 - employee is Present");
-//     }
-// }
+//UC1
+{
+    const IS_ABSENT = 0;
+    let empCheck = Math.floor(Math.random()*10)%2;
+    if (empCheck==IS_ABSENT){
+    console.log("UC1 - employee is Absent. Exiting the program");
+    return;
+    }else{
+      console.log("UC1 - employee is Present");
+    }
+}
 
-//UC 2 using switch case to calculate emp wage
+
 const IS_PART_TIME = 1;
 const IS_FULL_TIME = 2;
 const PART_TIME_HOURS = 4;
@@ -18,7 +18,7 @@ const FULL_TIME_HOURS = 8;
 const WAGE_PER_HOURS = 20;
 const NUM_OF_WORKING_DAY = 20;
 
-//UC3 Refactored code to write function for daily work hrs.
+//UC4 Calculate Wage For Month.
 
 function getWorkingHours(empCheck){
   switch(empCheck){
@@ -32,7 +32,9 @@ function getWorkingHours(empCheck){
     }
 }
 let empHr = 0;
-let empCheck = Math.floor(Math.random()*10)%3;
-empHr = getWorkingHours(empCheck);
+for (let day = 0; day< NUM_OF_WORKING_DAY; day++){
+  let empCheck = Math.floor(Math.random()*10)%3;
+  empHr += getWorkingHours(empCheck);
+}
 let empWage = empHr * WAGE_PER_HOURS;
-console.log("Emp Wage: "+ empWage);
+console.log("UC4-->> "+"Total Hrs: "+empHr+ " Emp Wage: "+ empWage);
